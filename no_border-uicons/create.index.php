b91e783e0274724258ac7cf1ee6d6f4b53c8e7e0
Wheel <jepke@users.noreply.github.com>
2021-05-09 23:54:33 +0000

    Create index inside reward subfolders

--- no_border-uicons/create.index.php
+++ no_border-uicons/create.index.php
@@ -10,6 +10,15 @@ foreach (dirtree($thisFolder) as $k => $dir) {
         $directory = dirtree($thisFolder . $k);
         file_put_contents($jsonFile, json_encode($directory));
     }
+    if ($k === 'reward') {
+        foreach ($dir as $ks => $subdir) {
+            if (is_dir($thisFolder . $k . DIRECTORY_SEPARATOR . $ks)) {
+                $jsonFile = $thisFolder . $k . DIRECTORY_SEPARATOR . $ks . DIRECTORY_SEPARATOR . 'index.json';
+                $directory = dirtree($thisFolder . $k . DIRECTORY_SEPARATOR . $ks);
+                file_put_contents($jsonFile, json_encode($directory));
+            }
+        }
+    }
 }
 
 function dirtree($dir, $ignoreEmpty=false) {
